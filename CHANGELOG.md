680f7840ba5fe17afd16b38b37392f04d75e7134
Axel Haustant <noirbizarre@gmail.com>
2019-01-05 17:49:18 +0100

    Update to version 0.1.1.dev for next development cycle

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## Current (in progress)
+
+- Nothing yet
+
 ## 0.1.0 (2019-01-05)
 
 Initial release
--- tox_asdf/__init__.py
+++ tox_asdf/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1.dev'
 __description__ = 'A plugin telling tox to use asdf to find python executables'
